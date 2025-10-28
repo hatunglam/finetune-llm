4c71f8ba67e054d932d50b3e4cc62109669d1dcf
ha-tung-lam-local <hatunglam2412@gmail.com>
2025-10-28 22:11:49 +0000

    complete fine tuning

--- finetune.py
+++ finetune.py
@@ -44,4 +44,65 @@ model = AutoModelForCausalLM.from_pretrained(
 model.config.use_cache = False
 model.config.pretraining_tp = 1
 
+# Loading tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+    model_name,
+    trust_remote_code=True,
+)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_size = 'right'
 
+# PEFT parameters
+peft_params = LoraConfig(
+    lora_alpha=16,
+    lora_dropout=0.1,
+    r=64,
+    bias='none',
+    task_type="CAUSAL_LM",  
+)
+
+# Training parameters
+training_params = TrainingArguments(
+    output_dir= "./result",
+    num_train_epochs=1,
+    per_device_eval_batch_size=4,
+    gradient_accumulation_steps=1,
+    optim="page_adamw_32bit",
+    save_steps=25,
+    learning_rate=2e-4,
+    weight_decay=0.001,
+    fp16=False,
+    bf16=False,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=0.03,
+    group_by_length=True,
+    lr_scheduler_type="constant",
+    report_to='tensorboard',
+)
+
+
+# Self supervised model tuning
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=dataset,
+    peft_config=peft_params,
+    dataset_text_field='text',
+    max_seq_length=None,
+    tokenizer=tokenizer,
+    args=training_params,
+    packing=False,
+)
+
+# Train model
+if __name__ == "__main__":
+    trainer.train()
+
+    trainer.model.save_pretrained(new_model)
+    trainer.tokenizer.save_pretrained(new_model)
+
+    # Evaluate with tensorboard
+
+    # from tensorboard import notebook
+    # log_dir = "results/runs"
+    # notebook.start("--logdir {} --port 4000".format(log_dir))
